d0ecc09d3a07a229c8dfc140eff00d8206a62ffa
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-11-03 10:31:06 +0000

    Bump version v0.1.1 -> v0.2.0

--- flake8_helper/__init__.py
+++ flake8_helper/__init__.py
@@ -36,7 +36,7 @@ __all__ = ["_V", "Visitor", "_P", "Plugin"]
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.1"
+__version__: str = "0.2.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 _P = TypeVar("_P", bound="Plugin")
